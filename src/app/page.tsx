b948a0dd45d3aa1667b85f99eb15fd95b40ab15f
Dymozz <224F6127@live.uem>
2026-02-06 22:07:36 +0100

    Limpiando marcas de conflicto en la página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,29 +18,21 @@ const HomePage: React.FC = () => {
         <Benefits />
 
         <Section
-<<<<<<< HEAD
+
           id="pricing"
           title="Tarifas"
           description="Precios claros y sin sorpresas. Paga solo por lo que necesitas."
-=======
-          id="Precios"
-          title="Precios"
-          description="Precios simples y transparentes. Sin sorpresas."
->>>>>>> f75138932eb102e03984542801c1bae77ff9fd9b
+
         >
           <Pricing />
         </Section>
 
         <Section
-<<<<<<< HEAD
+
           id="testimonials"
           title="Opiniones de nuestros clientes"
           description="Descubre cómo IoTCar ha mejorado la experiencia de nuestros usuarios."
-=======
-          id="Testimonios"
-          title="Qué Dicen Nuestros Clientes"
-          description="Escucha a quienes han confiado en nosotros."
->>>>>>> f75138932eb102e03984542801c1bae77ff9fd9b
+
         >
           <Testimonials />
         </Section>
